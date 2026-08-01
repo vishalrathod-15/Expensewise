c24c7787d1ade9c6a841814e02146db99ec9a3c2
Vishal Rathod <sdvishalrathod15@gmail.com>
2026-08-01 23:07:04 +0530

    chore: initial Spring Boot project setup with PostgreSQL connection

---
+++ src/main/java/com/expensewise/expensewise/ExpensewiseApplication.java
@@ -0,0 +1,13 @@
+package com.expensewise.expensewise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensewiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensewiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expensewise/expensewise/ExpensewiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expensewise.expensewise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensewiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
